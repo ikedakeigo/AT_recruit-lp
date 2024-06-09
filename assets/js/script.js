d0ce27b7465444f28177f728b1f7a7926a0b7cae
silverskg <silverskg555@gmail.com>
2024-06-09 17:32:03 +0900

    スタイル再調整

--- assets/js/script.js
+++ assets/js/script.js
@@ -72,12 +72,20 @@ jQuery(function ($) {
         $(".js-hamburger").click(function () {
         $(this).toggleClass("is-open");
         $(".js-drawer").toggleClass("is-open");
+        $('#mask').toggleClass('is-open');
+
+        if ($('body').css('overflow') === 'hidden') {
+            $('body').css({height: '', overflow: ''});
+        }else {
+            $('body').css({height: '100%', overflow: 'hidden'});
+        }
         });
 
         // ドロワーナビ
-        $(".js-drawer a[href]").on("click", function () {
+        $(".js-drawer").on("click", function () {
         $(".js-hamburger").removeClass("is-open");
         $(".js-drawer").removeClass("is-open");
+        $('#mask').removeClass('is-open');
         });
 
         // resizeイベント
@@ -89,7 +97,7 @@ jQuery(function ($) {
         });
 
         // スクロールイベント
-        $('#page-link a[href*="#"]').click(function () {
+        $('#page-link a[href*="#"], #header-link a[href*="#"]').click(function () {
         var elmHash = $(this).attr("href"); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
         var pos = $(elmHash).offset().top; //idの上部の距離を取得
         console.log(pos);
