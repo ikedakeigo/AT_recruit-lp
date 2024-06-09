f1a2b5fa42a218ae9264918631f7de56718eb87f
silverskg <silverskg555@gmail.com>
2024-06-09 16:23:34 +0900

    スタイルの調整終わり

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,36 +65,3 @@ window.onload = function () {
     resizeObserver.observe(guidelinenElement);
   }
 };
-
-jQuery(function ($) {
-  // ハンバーガーメニュー
-    $(function () {
-        $(".js-hamburger").click(function () {
-        $(this).toggleClass("is-open");
-        $(".js-drawer").toggleClass("is-open");
-        });
-
-        // ドロワーナビ
-        $(".js-drawer a[href]").on("click", function () {
-        $(".js-hamburger").removeClass("is-open");
-        $(".js-drawer").removeClass("is-open");
-        });
-
-        // resizeイベント
-        $(window).on("resize", function () {
-        if (window.matchMedia("(min-width: 800px)").matches) {
-            $(".js-hamburger").removeClass("is-open");
-            $(".js-drawer").removeClass("is-open");
-        }
-        });
-
-        // スクロールイベント
-        $('#page-link a[href*="#"]').click(function () {
-        var elmHash = $(this).attr("href"); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
-        var pos = $(elmHash).offset().top; //idの上部の距離を取得
-        console.log(pos);
-        $("body,html").animate({ scrollTop: pos - 160 }, 100); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
-        return false;
-        });
-    });
-});
--- src/js/script.js
+++ src/js/script.js
@@ -1,36 +1,32 @@
-jQuery(function ($) { // $はjQueryを表す
-    // この中にコードを書く
-
-    // 例：クラスがc-btnのcolorを黒色にする
-    // $(".c-btn").css("color","black")
-
-    // 例：idがbuttonのテキストを変更
-    // $("#button").text("ボタンのテキストを変更");
-
-    // 例：クラスがc-btnをクリックした場合、コンソールログを出力
-    // $(".c-btn").click(function() {
-    //     console.log("ボタンがクリックされました！");
-    // });
-
-    // ハンバーガーメニュー
-    $(function() {
-        $(".js-hamburger").click(function() {
-            $(this).toggleClass('is-open');
-            $(".js-drawer").toggleClass('is-open');
+jQuery(function ($) {
+  // ハンバーガーメニュー
+    $(function () {
+        $(".js-hamburger").click(function () {
+        $(this).toggleClass("is-open");
+        $(".js-drawer").toggleClass("is-open");
         });
 
         // ドロワーナビ
         $(".js-drawer a[href]").on("click", function () {
-            $(".js-hamburger").removeClass('is-open');
-            $('.js-drawer').removeClass('is-open');
+        $(".js-hamburger").removeClass("is-open");
+        $(".js-drawer").removeClass("is-open");
         });
 
         // resizeイベント
-        $(window).on('resize', function() {
-            if(window.matchMedia("(min-width: 800px)").matches) {
-                $(".js-hamburger").removeClass('is-open');
-                $('.js-drawer').removeClass('is-open');
-            }
-        })
-    })
+        $(window).on("resize", function () {
+        if (window.matchMedia("(min-width: 800px)").matches) {
+            $(".js-hamburger").removeClass("is-open");
+            $(".js-drawer").removeClass("is-open");
+        }
+        });
+
+        // スクロールイベント
+        $('#page-link a[href*="#"]').click(function () {
+        var elmHash = $(this).attr("href"); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
+        var pos = $(elmHash).offset().top; //idの上部の距離を取得
+        console.log(pos);
+        $("body,html").animate({ scrollTop: pos - 160 }, 100); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+        return false;
+        });
+    });
 });
