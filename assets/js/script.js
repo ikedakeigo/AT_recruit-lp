bbed51344e1cfd130013723125277711daa4b3a4
silverskg <silverskg555@gmail.com>
2024-09-07 17:45:55 +0900

    IntersectionObserverの設定 要素が画面ないに入ったか監視

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,46 @@
 jQuery(function ($) {
-  function delayScrollAnime() {
-    var time = 0.1; // 遅延時間を増やす秒数の値
-    var value = time;
-    $(".delay").each(function () {
-      var parent = this; // 親要素を取得
-      var elemPos = $(this).offset().top; // 要素の位置まで来たら
-      var scroll = $(window).scrollTop(); // スクロール値を取得
-      var windowHeight = $(window).height(); // 画面の高さを取得
-      var childs = $(this).children(); // 子要素を取得
+  function animateElements(entries, observer) {
+    entries.forEach(function (entry) {
+      if (entry.isIntersecting) {
+        var parent = $(entry.target);
+        var childs = parent.children();
 
-      // スクロールが要素の位置に達したか、またはそれを超えたかチェック
-      // かつ、スクロールが要素の下端よりも上にあるかチェック（要素が完全にビューポートから消えたらアニメーションを停止）
-      if (
-        scroll >= elemPos - windowHeight &&
-        scroll <= elemPos + $(parent).outerHeight() &&
-        !$(parent).hasClass("play")
-      ) {
-        $(childs).each(function () {
+        var time = 0.1; // 遅延時間の基本値
+        var value = time;
+
+        childs.each(function () {
           if (!$(this).hasClass("fadeUp")) {
-            $(parent).addClass("play"); // 親要素にクラス名playを追加
-            $(this).css("animation-delay", value + "s"); // アニメーション遅延のCSS animation-delayを追加
-            $(this).addClass("fadeUp"); // アニメーションのクラス名を追加
-            value = value + time; // delay時間を増加させる
-
-            var index = $(childs).index(this);
-            if (childs.length - 1 == index) {
-              $(parent).removeClass("play");
-            }
+            $(this).css("animation-delay", value + "s");
+            $(this).addClass("fadeUp");
+            value += time; // 遅延時間を増加させる
           }
         });
+
+        observer.unobserve(entry.target); // 一度アニメーションが実行されたら監視を解除
       }
     });
   }
-  // 画面をスクロールをしたら動かしたい場合の記述
-  jQuery(window).scroll(function () {
-    delayScrollAnime(); /* アニメーション用の関数を呼ぶ*/
-  }); // ここまで画面をスクロールをしたら動かしたい場合の記述
 
-  // 画面が読み込まれたらすぐに動かしたい場合の記述
-  jQuery(window).on("load", function () {
-    delayScrollAnime(); /* アニメーション用の関数を呼ぶ*/
-  }); // ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+  // IntersectionObserverの設定
+  var observer = new IntersectionObserver(animateElements, {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.1, // 要素が10%表示された時点でアニメーションを実行
+  });
+
+  // 各要素に対して監視をセット
+  $(".delay").each(function () {
+    observer.observe(this); // delayクラスを持つ要素を監視対象に
+  });
+
+  // ページが読み込まれた時にもアニメーションを適用
+  $(window).on("load", function () {
+    $(".delay").each(function () {
+      observer.observe(this);
+    });
+  });
 });
+
 
 window.onload = function () {
   var element = document.querySelector('[data-title="Value"]');
